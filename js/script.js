f33557a27cf1cda10bfe57a785cd693760e87e0b
Ashley Richard <ashleymrichard@gmail.com>
2022-03-25 00:05:42 -0700

    on click working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("JS Working!")
+APIKEY = "AIzaSyCkOlGAGRa1NLiz6t0P22ioh1xbbeY2YCw"
+const $input = $("input")
+const $button = $("button")
+
+$button.on("click", () =>{
+    let bookTitle = $input.val()
+    $.ajax({
+        url: `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}:keyes&${APIKEY}`
+    }).then((data) => {
+        console.log(data)
+    })
+})
