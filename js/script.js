5e0de7391d26d356eb8670ff7635ab617fc4f322
Ashley Richard <ashleymrichard@gmail.com>
2022-03-28 23:46:10 -0700

    li now connected to array / added in paragraph info

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 APIKEY = "AIzaSyCkOlGAGRa1NLiz6t0P22ioh1xbbeY2YCw"
 const $input = $("input")
 const $button = $("button")
+const $aside = $("aside")
 
-const books = []
+let books = []
 
 $button.on("click", () =>{
     let bookTitle = $input.val()
@@ -10,27 +11,50 @@ $button.on("click", () =>{
         url: `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}:keyes&${APIKEY}`
     }).then((data) => {
         // console.log(data)
+
         let items = $(data.items)
         let volumeInfo = $(data.volumeInfo)
         let title = $(data.title)
         
         for(let i = 0; i < items.length; i++) {
-             books.push(items[i].volumeInfo.title)
-            //   console.log(books)
-        }
-
-        const $ul = $("ul")
-
-        for (book of books) {
+            books.push(items[i])
+            console.log(books)
+            const $ul = $("ul")
             let $li = $("<li>")
-            $li.text(book)
-            $ul.append($li)
-        }
+            $li.text(items[i].volumeInfo.title)
+            $ul.append($li) 
+            }
 
-        let $li =$("li")
+        let $li = $("li")
 
-        $li.on("click", (event) => {
-            alert("Hello")
+        for(let i = 0; i < books.length; i++) {
+            $(document).ready(function() {
+            $('li').click(function() {
+                var index = $(this).index();
+               
+                const one = document.querySelector('#title1')
+                one.innerHTML = `Title: ${books[index].volumeInfo.title}`;
+
+                const two = document.querySelector('#author')
+                two.innerHTML = `Author(s): ${books[index].volumeInfo.authors}`;
+                if (books[index].volumeInfo.authors == null) {
+                    two.innerHTML = ""
+                }
+
+                const three = document.querySelector('#summary')
+                three.innerHTML = `Book Info: ${books[index].volumeInfo.description}`;
+                if (books[index].volumeInfo.description == null) {
+                    three.innerHTML = ""
+                }
+
+                $("aside").html(`<img src=${books[index].volumeInfo.imageLinks.smallThumbnail}/>`)
+                if (books[index].volumeInfo.imageLinks.smallThumbnail == null) {
+                    $aside.innerHTML = ""
+                }
+
+            })
         })
-    })
+    }
 })
+})
+
